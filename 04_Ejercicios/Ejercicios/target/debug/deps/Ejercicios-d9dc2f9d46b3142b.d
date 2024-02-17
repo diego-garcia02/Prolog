5ef08113e3126b87d0269338d2e4f9a8eb444674
LAPTOP-L5HBRREL\dgflo <l20120983@morelia.tecnm.mx>
2024-02-17 17:31:20 -0600

    Terminando tarea de funciones, areas y volumenes

--- 04_Ejercicios/Ejercicios/target/debug/deps/Ejercicios-d9dc2f9d46b3142b.d
+++
@@ -1,5 +0,0 @@
-C:\Prolog\04_Ejercicios\Ejercicios\target\debug\deps\Ejercicios-d9dc2f9d46b3142b.exe: src\main.rs
-
-C:\Prolog\04_Ejercicios\Ejercicios\target\debug\deps\Ejercicios-d9dc2f9d46b3142b.d: src\main.rs
-
-src\main.rs:
