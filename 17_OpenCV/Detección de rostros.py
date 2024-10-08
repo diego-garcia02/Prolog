963aa4745bfe5d2fd6aecd1729c36a7165c81e22
LAPTOP-L5HBRREL\dgflo <l20120983@morelia.tecnm.mx>
2024-10-08 10:57:49 -0600

    Binarizado de imagenes

--- 17_OpenCV/Detección de rostros.py
+++ 17_OpenCV/Detección de rostros.py
@@ -1,32 +1,81 @@
 import numpy as np
 import cv2 as cv
 
-rostro = cv.CascadeClassifier('./17_OpenCV/haarcascade_frontalface_alt.xml')
+#rostro = cv.CascadeClassifier('./17_OpenCV/haarcascade_frontalface_alt.xml')
 cap = cv.VideoCapture(0)
+
+lower_skin = np.array([0, 35, 0])
+upper_skin = np.array([20, 65, 255])
+
+
 x=y=w=h= 0 
 img = 0
 count = 0
 while True:
     ret, frame = cap.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    rostros = rostro.detectMultiScale(gray, 1.3, 5)
-    for(x, y, w, h) in rostros:
-        m= int(h/2)
-        #frame = cv.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        #frame = cv.rectangle(frame, (x,y+m), (x+w, y+h), (255, 0 ,0), 2 )
-
-        frame2 = frame[y:y+h, x:x+w]
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     
-        img = 180- frame[y:y+h,x:x+w]
-        count = count + 1   
+    mask = cv.inRange(hsv, lower_skin, upper_skin)
+
+    mask = cv.erode(mask,None, iterations=2)
+    mask = cv.dilate(mask,None,iterations=2)  
+
+    contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    if contours:
+        largest_contour = max(contours, key=cv.contourArea)
+        
+        # Encontrar el centro del contorno usando un círculo mínimo que lo rodee
+        ((x, y), radius) = cv.minEnclosingCircle(largest_contour)
+        
+        # Dibujar el círculo y el centro en el frame original si el radio es mayor que un umbral
+        if radius > 10:
+            #cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            #cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 255), -1)
+            cv.rectangle(frame, (int(x-radius), int(y-radius)), (int(x+radius), int(y+radius)), (0, 0, 255), 3)
+            img2 = frame[int(y-radius):int(y+radius),int(x-radius):int(x+radius)]
+
+            #Reescalando las imagenes a 80 y 100 
+            img_80 = cv.resize(img2, (80,80), interpolation=cv.INTER_AREA)
+            img_100 = cv.resize(img2, (100,100), interpolation=cv.INTER_AREA)
+
+            #Pasando las imagenes a escala de grises
+            gray_80 = cv.cvtColor(img_80, cv.COLOR_BGR2GRAY)
+            gray_100 = cv.cvtColor(img_100, cv.COLOR_BGR2GRAY)
+            
+            x80,y80 = gray_80.shape
+            x100,y100 = gray_100.shape
+
+            img80 = np.zeros((x80*2,y80*2),dtype='uint8')
+            img100 = np.zeros((x100*2,y100*2),dtype='uint8')
+
+            for i in range(x80):
+                for j in range(y80):
+                    if(img80[i,j]>150):
+                        img80[i, j]=255
+                    else:
+                        img80[i,j] = 0
+        
+            for i in range(x100):
+                for j in range(y100):
+                    if(img100[i,j]>150):
+                        img100[i, j]=255
+                    else:
+                        img100[i,j] = 0
+
+            cv.imshow('img80', img80)
+            cv.imshow('img100', img100)
+            
+            #cv2.imwrite("C://Prolog//17_OpenCV//Frames//recorte"+str(i)+".jpg",img2)
+            #cv2.imshow('img2',img2)
     
     #name = '/home/likcos/imgs/cara'+str(count)+'.jpg'
     #cv.imwrite(name, frame)
     cv.imshow('rostros', frame)
-    cv.imshow('cara', img)
+    cv.imshow('mask', mask)
     
-    k = cv.waitKey(1)
-    if k == 27:
+    k = cv.waitKey(1) 
+    if k == 27 & 0xFF == ord('q'):
         break
 cap.release()
 cv.destroyAllWindows()
--- 17_OpenCV/Seguimiento por color.py
+++ 17_OpenCV/Seguimiento por color.py
@@ -4,12 +4,11 @@ import numpy as np
 # Iniciar la captura de video desde la cámara
 cap = cv2.VideoCapture(0)
 
+
 # Definir el rango de color que quieres rastrear en el espacio de color HSV (en este caso, azul)
-lower_pink = np.array([140, 150, 0])
+lower_pink = np.array([140, 170, 0])
 upper_pink = np.array([170, 255, 255])
 
-lower_dark_pink = np.array([170,150,0])
-upper_dark_pink = np.array([180,255,255])
 i = 0
 while True:
     # Capturar frame por frame
@@ -20,19 +19,24 @@ while True:
     
     # Convertir el frame de BGR a HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower_pink, upper_pink)
+
+    mask = cv2.erode(mask,None, iterations=2)
+    mask = cv2.dilate(mask,None,iterations=2)
     
     # Crear una máscara que detecte solo el color azul
-    mask1 = cv2.inRange(hsv, lower_pink, upper_pink)
-    mask2 = cv2.inRange(hsv, lower_dark_pink, upper_dark_pink)
+    #mask1 = cv2.inRange(hsv, lower_pink, upper_pink)
+    #mask2 = cv2.inRange(hsv, lower_dark_pink, upper_dark_pink)
     
     # Filtrar la máscara con operaciones morfológicas
-    mask1 = cv2.erode(mask1, None, iterations=2)
-    mask1 = cv2.dilate(mask1, None, iterations=2)
+    #mask1 = cv2.erode(mask1, None, iterations=2)
+    #mask1 = cv2.dilate(mask1, None, iterations=2)
 
-    mask2 = cv2.erode(mask2, None, iterations=2)
-    mask2 = cv2.dilate(mask2, None, iterations=2)
+    #mask2 = cv2.erode(mask2, None, iterations=2)
+    #mask2 = cv2.dilate(mask2, None, iterations=2)
 
-    mask = mask1 + mask2
+    #mask = mask + mask2
     
     # Encontrar contornos en la máscara
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -49,12 +53,13 @@ while True:
         if radius > 10:
             #cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             #cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 255), -1)
-            #cv2.rectangle(frame,(int(x-radius), int(x+radius)) )
+            cv2.rectangle(frame, (int(x-radius), int(y-radius)), (int(x+radius), int(y+radius)), (0, 0, 255), 3)
+           
 
-
+            
             img2 = frame[int(y-radius):int(y+radius),int(x-radius):int(x+radius)]
-            cv2.imwrite("C://Prolog//17_OpenCV//Frames//recorte"+str(i)+".jpg",img2)
-            cv2.imshow('img2',img2)
+            #cv2.imwrite("C://Prolog//17_OpenCV//Frames//recorte"+str(i)+".jpg",img2)
+            #cv2.imshow('img2',img2)
     
     # Mostrar el frame
     cv2.imshow('Frame', frame)
